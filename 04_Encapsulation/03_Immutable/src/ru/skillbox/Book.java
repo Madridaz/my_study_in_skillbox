4ec7dab70beb3d929b609436f6a079726398b128
Александр Архипенков <archipenkov@tut.by>
2021-05-15 09:51:30 +0300

    Task 1 and 2, immutable module are ready

--- 04_Encapsulation/03_Immutable/src/ru/skillbox/Book.java
+++ 04_Encapsulation/03_Immutable/src/ru/skillbox/Book.java
@@ -1,2 +1,15 @@
-package ru.skillbox;public class Book {
+package ru.skillbox;
+
+public class Book {
+    private final String title;
+    private final String author;
+    private final int pages;
+    private final int numberISBN;
+
+    public Book(String title, String author, int pages, int numberISBN) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.numberISBN = numberISBN;
+    }
 }
