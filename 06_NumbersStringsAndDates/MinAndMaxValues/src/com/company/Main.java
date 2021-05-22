47dcd0742dc4a4074dd36a2f47671cbcfd967829
Александр Архипенков <archipenkov@tut.by>
2021-05-22 11:20:39 +0300

    task 6.2 done

---
+++ 06_NumbersStringsAndDates/MinAndMaxValues/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Минимальное значение byte: " + Byte.MIN_VALUE + "\n" +
+                "Максимальное значение byte: " + Byte.MAX_VALUE + "\n" +
+                "Минимальное значение short: " + Short.MIN_VALUE + "\n" +
+                "Максимальное значение short: " + Short.MAX_VALUE + "\n" +
+                "Минимальное значение int: " + Integer.MIN_VALUE + "\n" +
+                "Максимальное значение int: " + Integer.MAX_VALUE + "\n" +
+                "Минимальное значение long: " + Long.MIN_VALUE + "\n" +
+                "Максимальное значение long: " + Long.MAX_VALUE + "\n" +
+                "Минимальное значение float: " + Float.MIN_VALUE + "\n" +
+                "Максимальное значение float: " + Float.MAX_VALUE + "\n" +
+                "Минимальное значение double: " + Double.MIN_VALUE + "\n" +
+                "Максимальное значение double: " + Double.MAX_VALUE + "\n");
+    }
+}
