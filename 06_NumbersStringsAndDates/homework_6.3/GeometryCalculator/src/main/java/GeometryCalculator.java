c8586b34f24e87b16105bff1952fd26ccfa19d31
Александр Архипенков <archipenkov@tut.by>
2021-05-22 16:47:44 +0300

    task 6.3 done

--- 06_NumbersStringsAndDates/homework_6.3/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 06_NumbersStringsAndDates/homework_6.3/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,33 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        double square = Math.PI * Math.pow(radius, 2);
+        return Math.abs(square);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        double volume = (double) 4 / 3 * Math.PI * Math.pow(radius, 3);
+        return Math.abs(volume);
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
+        if (a + b > c && a + c > b && b + c > a) {
+            return true;
+        }
         return false;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        if (!isTrianglePossible(a, b, c)) {
+            return -1;
+        }
+        double p = (a + b + c) / 2;
+        double triangleSquare = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        return triangleSquare;
     }
 }
+
--- 06_NumbersStringsAndDates/homework_6.3/GeometryCalculator/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Площадь круга равна: " + GeometryCalculator.getCircleSquare(20));
+        System.out.println("Объем шара равен: " + GeometryCalculator.getSphereVolume(20));
+        System.out.println("Возможен ли такой треугольник: " + GeometryCalculator.isTrianglePossible(3, 4, 5));
+        System.out.println("Площадь треугольника: " + GeometryCalculator.getTriangleSquare(3, 4, 5));
+
+
     }
 }
