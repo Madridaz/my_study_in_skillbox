8886dafa3b7a3dd3427cde1ea2662889df17c707
Александр Архипенков <archipenkov@tut.by>
2021-05-24 09:05:03 +0300

    now we have negative min float and double

--- 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
+  public static void main(String[] args) {
+    System.out.println("Введите количество коробок");
+    Scanner scanner = new Scanner(System.in);
+    String boxes = scanner.nextLine();
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+    try {
+      int numberOfBoxes = Integer.valueOf(boxes);
+
+    } catch (NumberFormatException e) {
+      System.err.println("Неправильный формат строки!");
+    }
+
+
+    // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+    // пример вывода при вводе 2
+    // для отступа используйте табуляцию - \t
 
         /*
         Грузовик: 1
@@ -19,6 +28,6 @@ public class Main {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
-    }
+  }
 
 }
