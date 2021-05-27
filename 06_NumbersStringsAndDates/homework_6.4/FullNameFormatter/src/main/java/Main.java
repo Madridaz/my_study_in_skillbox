9d22dd4d5f4edccb3f98a3267fd145c5c1e0a651
Александр Архипенков <archipenkov@tut.by>
2021-05-27 14:26:17 +0300

    task 6.4.3

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -7,12 +7,25 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
-      if (input.equals("0")) {
+      if (input.equals("0") || input.equals(" ")) {
+        System.out.println("Введенная строка не является ФИО");
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      // TODO:напишите ваш код тут, результат вывести в консоль.
+      // При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      String surname = input.substring(0, input.indexOf(" "));
+      String name = input.substring(surname.length() + 1, input.lastIndexOf(" "));
+      String patronymic = input.substring((surname.length() + name.length() + 2), input.length());
+
+      if (surname.equals(" ") || name.equals(" ") || patronymic.equals(" ")) {
+        System.out.println("Введенная строка не является ФИО");
+      } else if (surname.isEmpty() || name.isEmpty() || patronymic.isEmpty()) {
+        System.out.println("Введенная строка не является ФИО");
+      } else {
+        System.out.println(
+            "Фамилия: " + surname + "\n" + "Имя: " + name + "\n" + "Отчество: " + patronymic);
+      }
     }
   }
-
 }
