1efaac1eab629b204f56a6068fe719797b279b29
Александр Архипенков <archipenkov@tut.by>
2021-05-27 15:56:19 +0300

    task 6.4 addon

--- 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912>", "****");
+  }
+
+  public static String searchAndReplaceDiamonds(String text, String placeholder) {
+    // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+    if (text.indexOf("<") != -1) {
+      int begin = text.indexOf("<");
+      int end = text.indexOf(">");
+      String secret = text.substring(begin + 1, end);
+      String safe = text.replaceAll(secret, placeholder);
+      System.out.println(safe);
+    } else {
+      System.out.println(text);
 
     }
-
-    public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
-    }
-
+    return text;
+  }
 }
