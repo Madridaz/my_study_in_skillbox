a3b67f35006b88287ca7f336a2b4797577f024e5
Александр Архипенков <archipenkov@tut.by>
2021-05-31 15:37:59 +0300

    task 6.5 addon ready

--- 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,25 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-    public static void main(String[] args) {
 
+  public static void main(String[] args) {
+    searchAndReplaceDiamonds("Номер кредитной карты <4008 1234> 56<4545>78 <5632 8912>", "****");
+
+
+  }
+
+  public static String searchAndReplaceDiamonds(String text, String placeholder) {
+    // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+    Pattern pattern = Pattern.compile("<.+?>");
+    Matcher matcher = pattern.matcher(text);
+    if (matcher.find()) {
+      String secret = text.replaceAll("<.+?>", placeholder);
+      System.out.println(secret);
     }
-
-    public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
-    }
-
+    return text;
+  }
 }
+
