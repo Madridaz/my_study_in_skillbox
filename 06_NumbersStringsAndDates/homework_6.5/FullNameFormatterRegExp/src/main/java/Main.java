6c6af9aa9a2d494e977e0158638f3c2119189981
Александр Архипенков <archipenkov@tut.by>
2021-05-31 08:50:24 +0300

    task 6.5.3 done

--- 06_NumbersStringsAndDates/homework_6.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-
     Scanner scanner = new Scanner(System.in);
+
     while (true) {
+      System.out.println("Введите данные");
+
       String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
+      boolean result =
+          input.matches("[А-Я{1}][а-я/-?]+\\s{1}[А-Я{1}][а-я/-?]+\\s{1}[А-Я{1}][а-я/-?]+");
+      if (result) {
+        String[] data = input.split(" ");
+
+        System.out.println("Фамилия " + data[0]);
+        System.out.println("Имя " + data[1]);
+        System.out.println("Отчество " + data[2] + "\n");
+
+      } else {
+        System.out.println("Введенная строка не является ФИО" + "\n");
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
   }
-
 }
--- 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 
   public static void main(String[] args) {
-
+    calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей");
   }
 
-  public static int calculateSalarySum(String text){
+  public static int calculateSalarySum(String text) {
     //TODO: реализуйте метод
+    String[] test = text.split("\\,");
+
     return -1;
   }
 
