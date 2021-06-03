68d15cbc5e36ed49cf9578a57c6411406368b5a5
Александр Архипенков <archipenkov@tut.by>
2021-06-03 13:13:12 +0300

    task 7.1.* is ready

--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
-    }
+
+  public static void main(String[] args) throws InterruptedException {
+    //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
+    // [X,  , X]
+    // [ , X,  ]
+    // [X,  , X]
+
+    TwoDimensionalArray.getTwoDimensionalArray(7);
+  }
 }
--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,14 +1,27 @@
 public class TwoDimensionalArray {
-    public static char symbol = 'X';
 
-    public static char[][] getTwoDimensionalArray(int size) {
+  public static char symbol = 'X';
 
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ symbol по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
+  public static char[][] getTwoDimensionalArray(int size) {
 
-        return new char[0][0];
+    char[][] array = {
+        {'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+        {' ', 'X', ' ', ' ', ' ', 'X', ' '},
+        {' ', ' ', 'X', ' ', 'X', ' ', ' '},
+        {' ', ' ', ' ', 'X', ' ', ' ', ' '},
+        {' ', ' ', 'X', ' ', 'X', ' ', ' '},
+        {' ', 'X', ' ', ' ', ' ', 'X', ' '},
+        {'X', ' ', ' ', ' ', ' ', ' ', 'X'}
+    };
+
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array.length; j++) {
+        System.out.print(array[i][j]);
+      }
+      System.out.println();
+
     }
+
+    return new char[0][0];
+  }
 }
