6f46785df906aa67f10dbb7814367931426b1c02
Александр Архипенков <archipenkov@tut.by>
2021-06-03 13:06:20 +0300

    task 7.1.2 is ready

--- 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
+++ 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
@@ -1,23 +1,53 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
 public class Hospital {
 
-    public static float[] generatePatientsTemperatures(int patientsCount) {
+  static float min = 32.0f;
+  static float max = 40.0f;
+  static float sum = 0.0f;
 
-        //TODO: напишите метод генерации массива температур пациентов
+  public static float[] generatePatientsTemperatures(int patientsCount) {
+    // TODO: напишите метод генерации массива температур пациентов
+    float[] array = new float[patientsCount];
 
-        return new float[0];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = (float) ((Math.random() * (max - min) + min));
+      //System.out.println(array[i]);
+      sum += array[i];
     }
+    return array;
+  }
 
-    public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-        */
+  public static String getReport(float[] temperatureData) {
+    /*
+    TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
+        а также температуры всех пациентов.
+    */
+    String temp = Arrays.toString(temperatureData);
 
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
-
-        return report;
+    int healthy = 0;
+    int sick = 0;
+    for (int i = 0; i < temperatureData.length; i++) {
+      if (temperatureData[i] >= 36.2 && temperatureData[i] <= 36.9) {
+        healthy++;
+      } else {
+        sick++;
+      }
     }
+    double roundOff = (double) Math.round(sum / temperatureData.length * 100) / 100;
+    String report =
+        "Температуры пациентов: "
+            + temp
+            + "\nСредняя температура: "
+            + roundOff/*sum / temperatureData.length*/
+            + "\nКоличество здоровых: "
+            + healthy;
+    return report;
+  }
 }
+
+//  chelovek v
+//  telege podskazal
+//  double value = 34.766674;
+//System.out.format("%.3f",value);
