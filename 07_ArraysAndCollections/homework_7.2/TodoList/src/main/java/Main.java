36ddcb0cd094d2eb02502186e5f2b2c045dad00e
Александр Архипенков <archipenkov@tut.by>
2021-06-06 10:03:44 +0000

    update task 7.2 (the code smells better)) )

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
@@ -1,65 +1,89 @@
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
+
+  public static void isOnlyAdd(String name) {
+    if (name.matches("(ADD)\\s\\D+")) {
+      System.out.println("Добавлено в конец списка");
+      String data = name.replace("ADD ", "");
+      todoList.add(data);
+    }
+  }
+
+  public static void addAndIndex(String name) {
+    if (name.matches("ADD\\s(\\d+)\\s(.+)")) {
+      System.out.println("Режим добавления по индексу");
+      String[] data = name.split(" ");
+      String index = data[1];
+      if (index.matches("\\d+")) {
+        int indexCut = Integer.parseInt(index);
+        try {
+          todoList.add(indexCut, data[2]);
+        } catch (IndexOutOfBoundsException exception) {
+          System.out.println("Введен несуществующий индекс, добавление будет в конец списка");
+          todoList.add(data[2]);
+        }
+      }
+    }
+  }
+
+  public static void editByIndex(String name) {
+    if (name.matches("EDIT\\s(\\d+)\\s(.+)")) {
+      System.out.println("Режим замены по индексу");
+      String[] data = name.split(" ");
+      String index = data[1];
+      if (index.matches("\\d+")) {
+        int indexCut = Integer.parseInt(index);
+        try {
+          todoList.edit(data[2], indexCut);
+        } catch (IndexOutOfBoundsException exception) {
+          System.out.println("Введен несуществующий индекс, замены не будет");
+        }
+      }
+    }
+  }
+
+  public static void deleteByIndex(String name) {
+    if (name.matches("DELETE\\s(\\d+)")) {
+      System.out.println("Режим удаления по индексу");
+      String[] data = name.split(" ");
+      String index = data[1];
+      if (index.matches("\\d+")) {
+        int indexCut = Integer.parseInt(index);
+        try {
+          todoList.delete(indexCut);
+        } catch (IndexOutOfBoundsException exception) {
+          System.out.println("Введен несуществующий индекс, удаления не будет");
+        }
+      }
+    }
+  }
 
   private static TodoList todoList = new TodoList();
 
   public static void main(String[] args) {
     // TODO: написать консольное приложение для работы со списком дел todoList
     System.out.println("Enter command: ");
+    System.out.println();
     Scanner scanner = new Scanner(System.in);
 
     while (true) {
       String name = scanner.nextLine();
-
-      if (name.matches("(ADD)\\s\\D+")) {
-        System.out.println("Добавлено в конец списка");
-        String data = name.replace("ADD ", "");
-        todoList.add(data);
-      } else if (name.matches("ADD\\s(\\d+)\\s(.+)")) {
-        System.out.println("Режим добавления по индексу");
-        String[] data = name.split(" ");
-        String index = data[1];
-        if (index.matches("\\d+")) {
-          int indexCut = Integer.parseInt(index);
-          try {
-            todoList.add(indexCut, data[2]);
-          } catch (IndexOutOfBoundsException exception) {
-            System.out.println("Введен несуществующий индекс, добавление будет в конец списка");
-            todoList.add(data[2]);
-          }
-        }
-      } else if (name.matches("EDIT\\s(\\d+)\\s(.+)")) {
-        System.out.println("Режим замены по индексу");
-        String[] data = name.split(" ");
-        String index = data[1];
-        if (index.matches("\\d+")) {
-          int indexCut = Integer.parseInt(index);
-          try {
-            todoList.edit(data[2], indexCut);
-          } catch (IndexOutOfBoundsException exception) {
-            System.out.println("Введен несуществующий индекс, замены не будет");
-          }
-        }
-      } else if (name.matches("DELETE\\s(\\d+)")) {
-        System.out.println("Режим удаления по индексу");
-        String[] data = name.split(" ");
-        String index = data[1];
-        if (index.matches("\\d+")) {
-          int indexCut = Integer.parseInt(index);
-          try {
-            todoList.delete(indexCut);
-          } catch (IndexOutOfBoundsException exception) {
-            System.out.println("Введен несуществующий индекс, удаления не будет");
-          }
-        }
-      }
-      if (name.contains("LIST")) {
-        todoList.getTodos();
+      String[] data = name.split(" ");
+      switch (data[0]) {
+        case "ADD":
+          isOnlyAdd(name);
+          addAndIndex(name);
+          break;
+        case "LIST":
+          todoList.getTodos();
+          break;
+        case "EDIT":
+          editByIndex(name);
+          break;
+        case "DELETE":
+          deleteByIndex(name);
       }
     }
   }
