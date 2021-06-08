28c6a95f2342e20d0cff6e7a2005835fde7902e5
Александр Архипенков <archipenkov@tut.by>
2021-06-08 20:17:08 +0300

    task 7.4

--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+  private static void checkInput(PhoneBook phoneBook) {
+    while (true) {
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+
+      if (input.matches("[A-ZА-ЯЁ]{1}[a-zа-яё]+")) { // введено имя
+        phoneBook.getPhonesByName(input);
+
+      } else if (input.matches(
+          "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$")) { //введен телефон
+        phoneBook.getNameByPhone(input);
+
+      } else if (input.contains("LIST")) {
+        phoneBook.getAllContacts();
+      }
     }
+  }
+
+  public static void main(String[] args) {
+    PhoneBook phoneBook = new PhoneBook();
+    System.out.println("Введите номер, имя или команду:");
+    checkInput(phoneBook);
+
+  }
 }
--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,29 +1,58 @@
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class PhoneBook {
 
-    public void addContact(String phone, String name) {
-        // проверьте корректность формата имени и телефона
-        // если такой номер уже есть в списке, то перезаписать имя абонента
-    }
+  TreeMap<String, String> contacts = new TreeMap<>();
 
-    public String getNameByPhone(String phone) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найдены - вернуть пустую строку
-        return "";
-    }
+  public void addContact(String name, String phone) { //работает
+    // проверьте корректность формата имени и телефона
+    // если такой номер уже есть в списке, то перезаписать имя абонента
+    contacts.put(name, phone);
+    System.out.println("Контакт сохранен.");
 
-    public Set<String> getPhonesByName(String name) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
-    }
+  }
 
-    public Set<String> getAllContacts() {
-        // формат одного контакта "Имя - Телефон"
-        // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+  public String getNameByPhone(String phone) {// выводит все пары а нужна искомая
+    // формат одного контакта "Имя - Телефон"
+    // если контакт не найдены - вернуть пустую строку
+    if (contacts.containsValue(phone)) {
+
+      Set set = contacts.entrySet();
+      for (Object element : set) {
+        Map.Entry mapEntry = (Map.Entry) element;
+        System.out.println(mapEntry.getKey() + " - " + mapEntry.getValue());
+      }
     }
+    return "";
+  }
+
+  public Set<String> getPhonesByName(String name) { //работает, но вывод в консоль не тот!
+    // формат одного контакта "Имя - Телефон"
+    // если контакт не найден - вернуть пустой TreeSet
+    if (contacts.containsKey(name)) {
+      System.out.println(contacts.ceilingEntry(name));
+    } else {
+      System.out.println("Такого имени в телефонной книге нет."
+          + "\n"
+          + "Введите номер телефона для абонента: " + name);
+      Scanner scanner = new Scanner(System.in);
+      String number = scanner.nextLine();
+      addContact(name, number);
+    }
+    return new TreeSet<>();
+  }
+
+  public Set<String> getAllContacts() { //разобраться с выводом в консоль и зачем TreeSet тут?
+    // формат одного контакта "Имя - Телефон"
+    // если контактов нет в телефонной книге - вернуть пустой TreeSet
+    TreeSet<String> allContacts = new TreeSet<>();
+    System.out.println(contacts);
+
+    return new TreeSet<>();
+  }
 
 }
