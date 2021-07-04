60203a7a019f5ce8d20cb5c5e0543d91193ab2b3
Александр Архипенков <archipenkov@tut.by>
2021-07-04 23:59:30 +0300

    задача 11.1 готова

--- 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
@@ -1,15 +1,30 @@
 import java.util.List;
+import net.sf.saxon.style.XSLOutput;
 
 public class Main {
 
-    public static final String STAFF_TXT = "data/staff.txt";
+  public static final String STAFF_TXT = "data/staff.txt";
 
-    public static void main(String[] args) {
-        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        System.out.println(staff);
-    }
+  public static void main(String[] args) {
+    List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+    System.out.println("начальный список");
+    System.out.println(staff);
+    System.out.println("список, отсортированный по зарплате и алфавиту");
+    sortBySalaryAndAlphabet(staff);
+  }
 
-    public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
-    }
+  public static void sortBySalaryAndAlphabet(List<Employee> staff) {
+
+    staff.sort((o1, o2) -> {
+      if (o1.getSalary().compareTo(o2.getSalary()) == 0) {
+        return o1.getName().compareTo(o2.getName());
+      } else {
+        return o1.getSalary().compareTo(o2.getSalary());
+      }
+    });
+    System.out.println(staff);
+
+  }
+
+
 }
