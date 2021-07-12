7b81cc44a9a716268ab96b86a1dbd145849dd45e
Александр Архипенков <archipenkov@tut.by>
2021-07-13 01:07:45 +0300

    airport

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -1,17 +1,31 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Flight.Type;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
 
+  public static void main(String[] args) {
 
-    }
+    System.out.println(findPlanesLeavingInTheNextTwoHours(Airport.getInstance()));
 
-    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
+  }
+
+  public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+    //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+    LocalTime currentTime = LocalTime.now();
+    return airport.getTerminals().stream()
+        .flatMap(terminal -> terminal.getFlights().stream())
+        .sorted(Comparator.comparing(Flight::getDate))
+        .filter(flight -> flight.getType() == Type.DEPARTURE)
+        .filter(flight -> flight.getDate().toInstant().atZone(ZoneId.systemDefault())
+            .toLocalTime().isAfter(currentTime))
+        .collect(Collectors.toList());
+  }
 
 }
