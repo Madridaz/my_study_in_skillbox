2a5bc69fc645abc9ba76c3c8f569d1b62737bcd1
Александр Архипенков <archipenkov@tut.by>
2021-07-31 19:01:21 +0300

    13.2

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,19 @@
+import com.google.common.io.Files;
+
+import java.io.File;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+        File source = new File(sourceDirectory);
+        File destination = new File(destinationDirectory);
+        try {
+            Files.copy(source, destination);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите откуда нужно скопировать");
+        String sourceFolder = reader.readLine();
+        System.out.println("Введите куда нужно скопировать");
+        String destinationFolder = reader.readLine();
+
+        System.out.println(sourceFolder + " " + destinationFolder);
+
+        FileUtils.copyFolder(sourceFolder, destinationFolder);
     }
 }
