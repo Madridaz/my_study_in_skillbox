cce72a96c42cea2e735e34ff48f0fb9026e3f60d
Александр Архипенков <archipenkov@tut.by>
2021-07-31 13:02:11 +0300

    задание 13.1

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        Path folder = Paths.get(path);
+        long size = 0;
+        try {
+            size = Files.walk(folder)
+                    .map(Path::toFile)
+                    .filter(File::isFile)
+                    .mapToLong(File::length)
+                    .sum();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
     }
+
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите путь до папки");
+        String s = reader.readLine();
+
+
+        System.out.println("Размер папки составляет: " + convertFolderSizeToMb(FileUtils.calculateFolderSize(s)) + " мегабайт");
+    }
+
+    public static double convertFolderSizeToMb(long size) {
+
+        return size / 1024 / 1024;
     }
 }
