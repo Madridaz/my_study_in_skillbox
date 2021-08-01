c7a0360cbd64fcb44ee206f31a0f6103908a58fe
Александр Архипенков <archipenkov@tut.by>
2021-08-01 17:42:48 +0300

    13.3

--- 13_FilesAndNetwork/homework_13.3/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.3/src/main/java/Main.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        final String FILE_TO_READ = "C:\\Users\\home\\IdeaProjects\\java_basics\\13_FilesAndNetwork\\files\\movementList.csv";
+        Movements movements = new Movements(FILE_TO_READ);
+
+
+        movements.readFile(FILE_TO_READ);
+        System.out.println("проверка");
+        System.out.println(movements.allLines);
+
+
+
+
     }
 }
--- 13_FilesAndNetwork/homework_13.3/src/main/java/Movements.java
+++ 13_FilesAndNetwork/homework_13.3/src/main/java/Movements.java
@@ -1,13 +1,80 @@
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Movements {
+
+    private String accountType;
+    private String accountNumber;
+    private String currency;
+    private String operationDate;
+    private String reference;
+    private String detailsOfOperation;
+    private double income;
+    private double expense;
+    List<Movements> allLines = new ArrayList<>();
+
+    public Movements(String accountType, String accountNumber, String currency, String operationDate, String reference, String detailsOfOperation, double income, double expense) {
+        this.accountType = accountType;
+        this.accountNumber = accountNumber;
+        this.currency = currency;
+        this.operationDate = operationDate;
+        this.reference = reference;
+        this.detailsOfOperation = detailsOfOperation;
+        this.income = income;
+        this.expense = expense;
+    }
+
+    @Override
+    public String toString() {
+        return "accountType: " + accountType + "\n" +
+                "accountNumber: " + accountNumber + "\n" +
+                "currency: " + currency + "\n" +
+                "operationDate: " + operationDate + "\n" +
+                "reference: " + reference + "\n" +
+                "detailsOfOperation: " + detailsOfOperation + "\n" +
+                "income: " + income + "\n" +
+                "expense: " + expense + "\n";
+    }
 
     public Movements(String pathMovementsCsv) {
     }
 
+    public void readFile(String file) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(file), ',', '"', 1);
+        String[] nextLine;
+
+        while ((nextLine = reader.readNext()) != null) {
+            if (nextLine != null) {
+                //System.out.println(Arrays.toString(nextLine));
+                nextLine[7].replace("\"", "");
+                nextLine[7].replace(",", ".");
+
+
+                try {
+                    allLines.add(new Movements(nextLine[0], nextLine[1], nextLine[2],
+                            nextLine[3], nextLine[4], nextLine[5], Double.parseDouble(nextLine[6]),
+                            Double.parseDouble(nextLine[7])));
+
+                } catch (NumberFormatException e) {
+                    System.err.println("дробное число не добавлено");
+                }
+            }
+        }
+    }
+
     public double getExpenseSum() {
+
         return 0.0;
     }
 
     public double getIncomeSum() {
+
         return 0.0;
     }
 }
+
