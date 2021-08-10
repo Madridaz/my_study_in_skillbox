097385732d189b1d7c111fa905aa46f8e0e6a4ca
Александр Архипенков <archipenkov@tut.by>
2021-08-10 14:40:25 +0300

    13.5

--- 13_FilesAndNetwork/homework_13_5/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13_5/src/main/java/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         MoscowSubway moscowSubway = new MoscowSubway();
 
-        System.out.println("номера и название линий");
-        System.out.println(moscowSubway.lines);
-        System.out.println();
-        System.out.println("номер линии и название станции");
+
+
         System.out.println(moscowSubway.stations);
 
 
--- 13_FilesAndNetwork/homework_13_5/src/main/java/MoscowSubway.java
+++ 13_FilesAndNetwork/homework_13_5/src/main/java/MoscowSubway.java
@@ -1,55 +1,80 @@
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class MoscowSubway {
-    final String WEB_SITE = "https://www.moscowmap.ru/metro.html#lines";
+    private static final String WEB_SITE = "https://www.moscowmap.ru/metro.html#lines";
+    private static final String FILENAME = "C:\\Users\\home\\IdeaProjects\\java_basics\\13_FilesAndNetwork\\homework_13_5\\src\\main\\resources\\MoscowSubwayMap.json";
+
     Document doc = getDocumentFromFile();
-    Map<String, String> lines = getAllLines();
-    Map<String, String> stations = getAllStations();
+    LinkedHashMap<String, String> lines = getAllLines();
+    LinkedHashMap<String, LinkedHashSet<String>> stations = getAllStations();
 
     public MoscowSubway() throws IOException {
     }
 
-
-//    //Получение кода страницы сайта метрополитена
+    //    //Получение кода страницы сайта метрополитена
 //    public Document getDocumentFromWeb() throws IOException {
 //        Document doc = Jsoup.connect(WEB_SITE).maxBodySize(0).get();
 //        return doc;
 //    }
 
     //Получение кода страницы из файла
-    public Document getDocumentFromFile() throws IOException {
-
+    private Document getDocumentFromFile() throws IOException {
         File htmlFile = new File("C:\\Users\\home\\IdeaProjects\\java_basics\\13_FilesAndNetwork\\homework_13_5\\src\\main\\resources\\info.html");
         Document doc = Jsoup.parse(htmlFile, "UTF-8");
         return doc;
     }
 
     //Получение всех линий метро
-    public Map<String, String> getAllLines() throws IOException {
+    private LinkedHashMap<String, String> getAllLines() throws IOException {
         Elements elements = getDocumentFromFile().select(".js-metro-line");
-        Map<String, String> lines = new HashMap<>();
+        LinkedHashMap<String, String> lines = new LinkedHashMap<>();
         for (int i = 0; i < elements.size(); i++) {
-            lines.put(elements.get(i).attr("data-line"), elements.get(i).text());
+            String key = elements.get(i).attr("data-line");
+            String value = elements.get(i).text();
+            lines.put(key, value);
         }
         return lines;
     }
 
     //Получение всех станций метро
-    public Map<String, String> getAllStations() throws IOException {
+    private LinkedHashMap<String, LinkedHashSet<String>> getAllStations() throws IOException {
         Elements elements = getDocumentFromFile().select(".js-metro-stations");
-        Map<String, String> stations = new HashMap<>();
+        LinkedHashMap<String, LinkedHashSet<String>> stations = new LinkedHashMap<>();
+        LinkedHashSet<String> group = new LinkedHashSet<>();
         for (int i = 0; i < elements.size(); i++) {
-            stations.put(elements.get(i).attr("data-line"), elements.get(i).text());
+            String key = elements.get(i).attr("data-line");
+            String value = elements.get(i).text().replaceAll("[0-9]+\\.", "");
+            String value1 = value.replaceAll("\\h", " ");
+            group.add(value1);
+            stations.put(key, group);
         }
         return stations;
     }
 
+    //создание JSON файла и сохранение его в папку
+    private void createJsonFile() {
+        JSONObject object = new JSONObject();
+        object.put("connections", "???");
+        object.put("stations", stations);
+        object.put("lines", lines);
+
+        try (
+                FileWriter writer = new FileWriter(FILENAME)) {
+            writer.write(object.toJSONString());
+            writer.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
 
