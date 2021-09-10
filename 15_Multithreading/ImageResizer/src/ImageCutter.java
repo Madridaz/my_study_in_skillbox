42f9ed5ba6689ce227fad089c9b9ee1492569c12
Александр Архипенков <archipenkov@tut.by>
2021-09-11 02:22:33 +0300

    15.1

---
+++ 15_Multithreading/ImageResizer/src/ImageCutter.java
@@ -0,0 +1,44 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageCutter extends Thread {
+    private File[] files;
+    private int newWidth;
+    private String dstFolder;
+    private long start;
+    private int targetWidth = 960;
+    private int targetHeight = 540;
+
+    public ImageCutter(File[] files, int newWidth, String dstFolder, long start) {
+        this.files = files;
+        this.newWidth = newWidth;
+        this.dstFolder = dstFolder;
+        this.start = start;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (File file : files) {
+                BufferedImage image = ImageIO.read(file);
+                if (image == null) {
+                    continue;
+                }
+                Image resultingImage = image.getScaledInstance(targetWidth, targetHeight, Image.SCALE_AREA_AVERAGING);
+                BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+                outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+
+                File newFile = new File(dstFolder + "/" + file.getName());
+                ImageIO.write(outputImage, "jpg", newFile);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Image.getScaledInstance() used");
+        System.out.println("Cutting finished after: " + (System.currentTimeMillis() - start) + " ms.");
+
+
+    }
+}
--- 15_Multithreading/ImageResizer/src/Main.java
+++ 15_Multithreading/ImageResizer/src/Main.java
@@ -1,51 +1,28 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Main {
+    private static int newWidth = 300;
 
     public static void main(String[] args) {
-        String srcFolder = "/users/sortedmap/Desktop/src";
-        String dstFolder = "/users/sortedmap/Desktop/dst";
+        String srcFolder = "C:\\Users\\home\\Desktop\\src";
+        String dstFolder = "C:\\Users\\home\\Desktop\\dst";
 
         File srcDir = new File(srcFolder);
 
         long start = System.currentTimeMillis();
 
+        //Intel(R) Pentium(R) CPU 4405U @ 2.10GHz содержит 2 ядра
         File[] files = srcDir.listFiles();
+        int middle = files.length / 2;
 
-        try {
-            for (File file : files) {
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    continue;
-                }
+        File[] coreOne = new File[middle];
+        System.arraycopy(files, 0, coreOne, 0, coreOne.length);
+        ImageCutter imageCutterCoreOne = new ImageCutter(coreOne, newWidth, dstFolder, start);
+        imageCutterCoreOne.start();
 
-                int newWidth = 300;
-                int newHeight = (int) Math.round(
-                    image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                    newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
-
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
+        File[] coreTwo = new File[files.length - middle];
+        System.arraycopy(files, middle, coreTwo, 0, coreTwo.length);
+        ImageCutter imageCutterCoreTwo = new ImageCutter(coreTwo, newWidth, dstFolder, start);
+        imageCutterCoreTwo.start();
     }
 }
