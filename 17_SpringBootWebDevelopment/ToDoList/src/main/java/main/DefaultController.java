61f9e9abb7d9f07e45fc58cd8d543dcb14b3bbc7
archipenkov@tut.by <4uEYjUg9s!u85LN>
2021-10-16 15:03:26 +0300

    12

--- 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,16 +1,20 @@
 package main;
 
-import java.util.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
 
+  private final String SHOWALL = "Получение всего списка дел - /notes/";
+  private final String ADD = "Добавление дела - /notes/new";
+  private final String DELETE = "Удаление дела №0 - /notes/delete/new";
+  private final String CLEAR = "Удаление всего списка дел - /notes/clear";
+
   @RequestMapping("/")
   public String index() {
-    double i = Math.random();
-    return (new Date()).toString() + "; " + "случайное число " + i;
+
+    return SHOWALL + " " + ADD + " " + DELETE + " " + CLEAR;
   }
 
 
