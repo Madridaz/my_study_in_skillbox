e6fe3f37373aab6d9acc8aae735c311982935215
archipenkov@tut.by <4uEYjUg9s!u85LN>
2021-11-24 19:28:30 +0300

    21.1

--- 20_Performance/CarNumberGenerator/src/Loader.java
+++ 20_Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,46 @@
-import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class Loader {
 
-    public static void main(String[] args) throws Exception {
-        long start = System.currentTimeMillis();
+  public static void main(String[] args) throws Exception {
+    long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+    PrintWriter writer = new PrintWriter("res/numbers.txt");
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
+    char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+
+    for (int regionCode = 1; regionCode < 100; regionCode++) {
+      StringBuilder builder = new StringBuilder();
+      for (int number = 1; number < 100; number++) {
+        for (char firstLetter : letters) {
+          for (char secondLetter : letters) {
+            for (char thirdLetter : letters) {
+              builder.append(firstLetter);
+              builder.append(padNumber(number, 3));
+              builder.append(secondLetter);
+              builder.append(thirdLetter);
+              builder.append(padNumber(regionCode, 2));
+              builder.append("\n");
             }
+          }
         }
+      }
+      writer.write(builder.toString());
+    }
+    writer.flush();
+    writer.close();
 
-        writer.flush();
-        writer.close();
+    System.out.println((System.currentTimeMillis() - start) + " ms");
+  }
 
-        System.out.println((System.currentTimeMillis() - start) + " ms");
+  private static String padNumber(int number, int numberLength) {
+    String numberStr = Integer.toString(number);
+    int padSize = numberLength - numberStr.length();
+
+    for (int i = 0; i < padSize; i++) {
+      numberStr = '0' + numberStr;
     }
 
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
-        }
-
-        return numberStr;
-    }
+    return numberStr;
+  }
 }
